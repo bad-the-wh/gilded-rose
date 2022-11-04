23bbe06ed83e944faf74e416a81adac8d34b016a
Minimacaco59 <favre.alex@gmail.com>
2022-11-04 16:39:33 +0100

    Eventupdate

--- src/main/java/gildedrose/EventItem.java
+++ src/main/java/gildedrose/EventItem.java
@@ -1,5 +1,7 @@
 package gildedrose;
 
+import javax.lang.model.util.ElementScanner14;
+
 public class EventItem extends Item{
 
     public EventItem(int sellIn, int quality) {
@@ -10,6 +12,20 @@ public class EventItem extends Item{
 
     @Override
     public void update() {
-
+        if (this.getSellIn() > 10 ) {
+            this.setSellIn(this.getSellIn()-1);
+            if (this.getQuality() < 50) {
+                this.setQuality(this.getQuality() + 1);
+            }
+        }
+        else {
+            if (this.getSellIn() == 0 ){
+                this.setQuality(0);
+            }
+            else {
+                this.setQuality(this.getQuality() + 2);
+                this.setSellIn(this.getSellIn()-1);
+            }
+        }
     }
 }
--- src/test/java/gildedrose/GildedRoseTest.java
+++ src/test/java/gildedrose/GildedRoseTest.java
@@ -46,4 +46,29 @@ class GildedRoseTest {
         assertEquals(4, shop.items.get(2).getSellIn());
     }
 
+    @Test
+    void should_update_event_quality() {
+        ArrayList<Item> items = new ArrayList<Item>();
+        items.add(new EventItem(5, 8));
+        items.add(new EventItem(0, 8));
+        items.add(new EventItem(20, 50));
+        items.add(new EventItem(20, 49));
+        items.add(new EventItem(20, 30));
+        Shop shop = new Shop(items);
+        items.get(0).update();
+        items.get(1).update();
+        items.get(2).update();
+        items.get(3).update();
+        items.get(4).update();
+        assertEquals(10, shop.items.get(0).getQuality());
+        assertEquals(4, shop.items.get(0).getSellIn());
+        assertEquals(0, shop.items.get(1).getQuality());
+        assertEquals(0, shop.items.get(1).getSellIn());
+        assertEquals(50, shop.items.get(2).getQuality());
+        assertEquals(19, shop.items.get(2).getSellIn());
+        assertEquals(50, shop.items.get(3).getQuality());
+        assertEquals(19, shop.items.get(3).getSellIn());
+        assertEquals(31, shop.items.get(4).getQuality());
+        assertEquals(19, shop.items.get(4).getSellIn());
+    }
 }
