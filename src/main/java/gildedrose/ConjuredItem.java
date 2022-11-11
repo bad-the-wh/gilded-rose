4f4ae50299cddd7500ba29b8ddfaf61fe58b20b3
Minimacaco59 <favre.alex@gmail.com>
2022-11-11 12:09:58 +0100

    simplification code conjureditem

--- src/main/java/gildedrose/ConjuredItem.java
+++ src/main/java/gildedrose/ConjuredItem.java
@@ -3,47 +3,18 @@ package gildedrose;
 public class ConjuredItem extends Item{
 
     public ConjuredItem(int sellIn, int quality) {
-
         super(sellIn, quality);
-
     }
 
     @Override
     public void update() {
+        this.sellIn--;
+        this.quality=this.quality - 2 ;
 
-        if(this.getSellIn() > 0){
-
-            if(this.getQuality() >1){
-
-                this.setSellIn(this.getSellIn() - 1);
-                this.setQuality(this.getQuality() - 2);
-
-            }
-
-            else{
-
-                this.setSellIn(this.getSellIn() - 1);
-                this.setQuality(0);
-            }
-
-        }
-
-        else {
-
-            if(this.getQuality() > 3){
-
-                this.setQuality(this.getQuality() - 4);
-
-            }
-
-            else{
-
-                this.setQuality(0);
-
-            }
-
-        }
-
-
+        if (this.sellIn < 0)
+            this.quality= this.quality - 2;
+        
+        if (this.quality < 0)
+            this.quality = 0;
     }
 }
