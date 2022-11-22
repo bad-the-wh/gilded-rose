14f4f3a92c60b9233d5695fc6e221d8d4a2d8d51
Minimacaco59 <favre.alex@gmail.com>
2022-11-22 11:21:14 +0100

    FileItemsRepository

---
+++ src/main/java/gildedrose/FileItemsRepository.java
@@ -0,0 +1,19 @@
+package gildedrose;
+
+import java.util.List;
+
+public class FileItemsRepository implements ItemsRepository{
+
+    @Override
+    public List<Item> getInventory() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void saveInventory(List<Item> items) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
