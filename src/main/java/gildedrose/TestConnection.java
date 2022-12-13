335dfff94640a417887c972fcfe9f949f6c817f6
Minimacaco59 <117276158+Minimacaco59@users.noreply.github.com>
2022-12-14 00:00:23 +0100

    test select dans test connection

--- src/main/java/gildedrose/TestConnection.java
+++ src/main/java/gildedrose/TestConnection.java
@@ -1,14 +1,18 @@
 package gildedrose;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+
 
 public class TestConnection {
     public static void main(String... args){
         Connection conn = null;
         try {
             conn = DriverManager.getConnection("jdbc:mysql://epsi-tp001.cdtv1lsfjbfz.eu-west-3.rds.amazonaws.com/GILDEROSE?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris","admin","EPSITP01");
+            PreparedStatement preparedStatement=conn.prepareStatement("SELECT * FROM  ITEMS");
+            ResultSet item = preparedStatement.executeQuery();
+            while(item.next()){
+                System.out.println(item.getString("TYPE"));
+            }          
             System.out.println("success");
         } catch (SQLException e) {
             e.printStackTrace();
