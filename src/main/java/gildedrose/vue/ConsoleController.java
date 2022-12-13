9604d28e25012048439f8cd52f4bb16c6a1e5bc5
ANDREL-DAVY DE PRA BIGOMBE <10175767@leroymerlin.fr>
2022-12-13 14:32:58 +0100

    TP10

--- src/main/java/gildedrose/vue/ConsoleController.java
+++ src/main/java/gildedrose/vue/ConsoleController.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 @Setter
 public class ConsoleController {
 
-    private ShopInputBoundary shopInputBoundary;
+    private final ShopInputBoundary shopInputBoundary = new ShopInputBoundary();
 
 
 
