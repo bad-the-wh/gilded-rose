7ff872c817f1b3a3cc7f45994a69b1e4fc5759e6
Minimacaco59 <117276158+Minimacaco59@users.noreply.github.com>
2022-12-14 11:38:55 +0100

    finditem in db

--- src/main/java/gildedrose/persistance/DbItemsGateway.java
+++ src/main/java/gildedrose/persistance/DbItemsGateway.java
@@ -22,14 +22,32 @@ public class DbItemsGateway implements ItemsGateway{
     
     @Override
     public List<Item> getInventory(){
-        List<Item> items = null;
         Connection conn = null;
         try {
             conn = DriverManager.getConnection("jdbc:mysql://epsi-tp001.cdtv1lsfjbfz.eu-west-3.rds.amazonaws.com/GILDEROSE?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris","admin","EPSITP01");
             PreparedStatement preparedStatement=conn.prepareStatement("SELECT * FROM  ITEMS");
-            ResultSet item = preparedStatement.executeQuery();
-            while(item.next()){
-                items.add(item.getString("TYPE"),item.getInt("SELLIN"),item.getInt("QUALITY"),item.getInt("BASEPRICE"));
+            ResultSet line = preparedStatement.executeQuery();
+            
+            while(line.next()){
+                String type = line.getString("TYPE");
+                int sellin = line.getInt("SELLIN");
+                int quality = line.getInt("QUALITY");
+                int basePrice = line.getInt("BASEPRICE");
+                if(type == "LegendaryItem"){
+                    items.add(new LegendaryItem(sellin,quality,basePrice));
+                }
+                else if(type == "AgingItem"){
+                    items.add(new AgingItem(sellin,quality,basePrice));
+                }
+                else if(type == "EventItem"){
+                    items.add(new EventItem(sellin,quality,basePrice));
+                }
+                else if(type == "ConjuredItem"){
+                    items.add(new ConjuredItem(sellin,quality,basePrice));
+                }
+                else{
+                    items.add(new GenericItem(sellin,quality,basePrice));
+                }
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -48,14 +66,69 @@ public class DbItemsGateway implements ItemsGateway{
 
     @Override
     public void saveInventory(List<Item> items) {
-        // TODO Auto-generated method stub
         
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://epsi-tp001.cdtv1lsfjbfz.eu-west-3.rds.amazonaws.com/GILDEROSE?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris","admin","EPSITP01");
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if (conn!=null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public Item findItem(String type, int quality)  {
-        // TODO Auto-generated method stub
-        return null;
+        Item item = null;
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://epsi-tp001.cdtv1lsfjbfz.eu-west-3.rds.amazonaws.com/GILDEROSE?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris","admin","EPSITP01");
+            PreparedStatement preparedStatement=conn.prepareStatement("SELECT * FROM  ITEMS WHERE TYPE=? AND QUALITY=?");
+            preparedStatement.setString(1, type);
+            preparedStatement.setInt(2, quality);
+            ResultSet line = preparedStatement.executeQuery();
+            if (line != null ){
+                int sellin = line.getInt("SELLIN");
+                int basePrice = line.getInt("BASEPRICE");
+                if(type == "LegendaryItem"){
+                    items.add(new LegendaryItem(sellin,quality,basePrice));
+                }
+                else if(type == "AgingItem"){
+                    items.add(new AgingItem(sellin,quality,basePrice));
+                }
+                else if(type == "EventItem"){
+                    items.add(new EventItem(sellin,quality,basePrice));
+                }
+                else if(type == "ConjuredItem"){
+                    items.add(new ConjuredItem(sellin,quality,basePrice));
+                }
+                else{
+                    items.add(new GenericItem(sellin,quality,basePrice));
+                }
+            }else{
+                // item non trouvé dans la base 
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if (conn!=null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     }
 
     @Override
