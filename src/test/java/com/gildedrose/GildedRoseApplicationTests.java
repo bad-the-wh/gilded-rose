8e195e7182fa225d1d2a28162af57c7d5e34e4dd
Minimacaco59 <117276158+Minimacaco59@users.noreply.github.com>
2022-12-18 11:35:44 +0100

    remise en place des tests

--- src/test/java/com/gildedrose/GildedRoseApplicationTests.java
+++ src/test/java/com/gildedrose/GildedRoseApplicationTests.java
@@ -1,6 +1,7 @@
 package com.gildedrose;
 
 import com.gildedrose.core.domain.ItemsGateway;
+import com.gildedrose.core.domain.SellItemRequest;
 import com.gildedrose.core.domain.ShopInteractor;
 import com.gildedrose.core.domain.item.*;
 import org.junit.jupiter.api.BeforeEach;
@@ -65,7 +66,7 @@ class GildedRoseApplicationTests {
 
 	@Test
 	void should_not_decrease_quality_under_zero()  {
-		Item item = GenericItem.builder().id(UUID.randomUUID().toString()).itemName("GenericItem").sellIn(5).quality(8).basePrice(10).build();
+		Item item = GenericItem.builder().id(UUID.randomUUID().toString()).itemName("GenericItem").sellIn(5).quality(0).basePrice(10).build();
 		items.add(item);
 
 		shop.getRepository().saveInventory(items);
@@ -73,66 +74,143 @@ class GildedRoseApplicationTests {
 
 		assertEquals(0, shop.getRepository().findItemsBy().get(0).getQuality());
 	}
-/*
-	@Test
-	void should_increase_agedbrie_quality()  {
-		assertEquals(9, repository.findItemsBy().get(3).getQuality());
-	}
 
 	@Test
+	void should_increase_agedbrie_quality()  {
+		Item item = AgingItem.builder().id(UUID.randomUUID().toString()).itemName("AgingItem").sellIn(5).quality(8).basePrice(10).build();
+		items.add(item);
+
+		shop.getRepository().saveInventory(items);
+		shop.updateQuality();
+
+		assertEquals(9, shop.getRepository().findItemsBy().get(0).getQuality());
+		
+	}
+
+ 	@Test
 	void should_not_increase_quality_over_fity()  {
-		assertEquals(50, repository.findItemsBy().get(4).getQuality());
+		Item item = AgingItem.builder().id(UUID.randomUUID().toString()).itemName("AgingItem").sellIn(5).quality(50).basePrice(10).build();
+		items.add(item);
+
+		shop.getRepository().saveInventory(items);
+		shop.updateQuality();
+
+		assertEquals(50, shop.getRepository().findItemsBy().get(0).getQuality());
+		
 	}
 
 	@Test
 	void should_not_changed_sulfuras_quality()  {
-		assertEquals(80, repository.findItemsBy().get(5).getQuality());
+		Item item = LegendaryItem.builder().id(UUID.randomUUID().toString()).itemName("LegendaryItem").sellIn(5).quality(50).basePrice(10).build();
+		items.add(item);
+
+		shop.getRepository().saveInventory(items);
+		shop.updateQuality();
+
+		assertEquals(80, shop.getRepository().findItemsBy().get(0).getQuality());
+		
 	}
 
 	@Test
 	void should_increase_event_item_quality()  {
-		assertEquals(9, repository.findItemsBy().get(6).getQuality());
-		assertEquals(50, repository.findItemsBy().get(7).getQuality());
+		Item item = EventItem.builder().id(UUID.randomUUID().toString()).itemName("EventItem").sellIn(12).quality(8).basePrice(10).build();
+		items.add(item);
+
+		shop.getRepository().saveInventory(items);
+		shop.updateQuality();
+
+		assertEquals(9, shop.getRepository().findItemsBy().get(0).getQuality());
+		
+		
 	}
 
 	@Test
 	void should_increase_event_item_quality_ten_days_before()  {
-		assertEquals(35, repository.findItemsBy().get(8).getQuality());
+		Item item = EventItem.builder().id(UUID.randomUUID().toString()).itemName("EventItem").sellIn(9).quality(33).basePrice(10).build();
+		items.add(item);
+
+		shop.getRepository().saveInventory(items);
+		shop.updateQuality();
+
+		assertEquals(35, shop.getRepository().findItemsBy().get(0).getQuality());
+		
 	}
 
 	@Test
 	void should_increase_event_item_quality_five_days_before()  {
-		assertEquals(24, repository.findItemsBy().get(9).getQuality());
+		Item item = EventItem.builder().id(UUID.randomUUID().toString()).itemName("EventItem").sellIn(4).quality(21).basePrice(10).build();
+		items.add(item);
+
+		shop.getRepository().saveInventory(items);
+		shop.updateQuality();
+
+		assertEquals(24, shop.getRepository().findItemsBy().get(0).getQuality());
+		
 	}
 
 	@Test
 	void should_increase_event_item_quality_after_event()  {
-		assertEquals(0, repository.findItemsBy().get(10).getQuality());
+		Item item = EventItem.builder().id(UUID.randomUUID().toString()).itemName("EventItem").sellIn(0).quality(21).basePrice(10).build();
+		items.add(item);
+
+		shop.getRepository().saveInventory(items);
+		shop.updateQuality();
+
+		assertEquals(0, shop.getRepository().findItemsBy().get(0).getQuality());
+		
 	}
 
 	@Test
 	void should_update_conjured_quality_twice_as_fast_as_generic()  {
-		assertEquals(6, repository.findItemsBy().get(11).getQuality());
-		assertEquals(4, repository.findItemsBy().get(12).getQuality());
-		assertEquals(0, repository.findItemsBy().get(13).getQuality());
+		Item item = ConjuredItem.builder().id(UUID.randomUUID().toString()).itemName("ConjuredItem").sellIn(5).quality(8).basePrice(10).build();
+		items.add(item);
+		Item item1 = ConjuredItem.builder().id(UUID.randomUUID().toString()).itemName("ConjuredItem").sellIn(0).quality(8).basePrice(10).build();
+		items.add(item1);
+		Item item2 = ConjuredItem.builder().id(UUID.randomUUID().toString()).itemName("ConjuredItem").sellIn(0).quality(3).basePrice(10).build();
+		items.add(item2);
+
+		shop.getRepository().saveInventory(items);
+		shop.updateQuality();
+
+		assertEquals(6, shop.getRepository().findItemsBy().get(0).getQuality());
+		assertEquals(4, shop.getRepository().findItemsBy().get(1).getQuality());
+		assertEquals(0, shop.getRepository().findItemsBy().get(2).getQuality());
+		
 	}
 
 	@Test
 	void should_get_item_value(){
-		assertEquals(510,repository.findItemsBy().get(4).getValue() );
+		Item item = AgingItem.builder().id(UUID.randomUUID().toString()).itemName("AgingItem").sellIn(5).quality(50).basePrice(10).build();
+		items.add(item);
+
+		shop.getRepository().saveInventory(items);
+		shop.updateQuality();
+		assertEquals(510, shop.getRepository().findItemsBy().get(0).getValue());
+		
 	}
 
-	@Test
+/* 	@Test
 	void should_found_item_in_inventory(){
-		assertEquals(itemNeeded.getClass(),repository.findItemByItemNameAndQuality("GenericItem", 7).getClass());
+		Item item = AgingItem.builder().id(UUID.randomUUID().toString()).itemName("AgingItem").sellIn(5).quality(50).basePrice(10).build();
+		items.add(item);
+
+		shop.getRepository().saveInventory(items);
+		String name = item.getItemName();
+		int quality = item.getQuality();
+
+		assertEquals(item, shop.getRepository().findItemByItemNameAndQuality(name, quality));
+	}
+*/
+	@Test
+	void should_update_shop_solde_after_selling_item(){
+		Item item = GenericItem.builder().id(UUID.randomUUID().toString()).itemName("GenericItem").sellIn(5).quality(8).basePrice(10).build();
+		items.add(item);
+		shop.getRepository().saveInventory(items);
+		SellItemRequest request = new SellItemRequest(item.getItemName(), item.getQuality());
+
+	     assertEquals(80,shop.getRepository().sellItem(request));
 
 	}
-
-	//@Test
-	//void should_update_shop_solde_after_selling_item(){
-	//     assertEquals(80,shop.sellItem("GenericItem",7));
-
-	//}
 
 	//@Test
 	//void should_warning_in_case_of_item_not_found() throws ItemNotFoundException {
